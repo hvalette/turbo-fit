fe5d463695b874af5e4f9b49d7018830b5eb3cf2
Henri Valette <henri.valette.fr@gmail.com>
2025-01-27 11:14:28 +0100

    fix: challenges

--- src/app/api/challenges/route.ts
+++ src/app/api/challenges/route.ts
@@ -3,15 +3,15 @@ import { NextResponse } from 'next/server';
 
 export async function GET() {
   const today = new Date();
-  const threeDaysLater = new Date();
-  threeDaysLater.setDate(today.getDate() + 3);
+  const threeDaysAgo = new Date();
+  threeDaysAgo.setDate(today.getDate() - 3);
   const challenge = await prisma.challenge.findMany({
     where: {
       startDate: {
         lte: today,
       },
       endDate: {
-        gte: threeDaysLater,
+        gte: threeDaysAgo,
       },
     },
   });
